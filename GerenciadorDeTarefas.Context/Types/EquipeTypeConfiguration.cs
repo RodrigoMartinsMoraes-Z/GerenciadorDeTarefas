02c9d30975825f5b1b222c96f47a8183ec66bebe
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2020-08-20 20:58:53 -0300

    type configuration de equipe

---
+++ GerenciadorDeTarefas.Context/Types/EquipeTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using GerenciadorDeTarefas.Domain.Equipes;
+using GerenciadorDeTarefas.Domain.Usuarios;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorDeTarefas.Context.Types
+{
+    class EquipeTypeConfiguration : IEntityTypeConfiguration<Equipe>
+    {
+        public void Configure(EntityTypeBuilder<Equipe> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.HasMany(e => e.Projetos).WithOne(p => p.Equipe).HasForeignKey(p => p.Id);
+        }
+    }
+}
---
+++ GerenciadorDeTarefas.Context/Types/ManyToMany/EquipeUsuarioTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using GerenciadorDeTarefas.Domain.ManyToMany;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorDeTarefas.Context.Types.ManyToMany
+{
+    class EquipeUsuarioTypeConfiguration : IEntityTypeConfiguration<EquipeUsuario>
+    {
+        public void Configure(EntityTypeBuilder<EquipeUsuario> builder)
+        {
+            builder.HasKey(eu => new { eu.IdEquipe, eu.IdUsuario });
+
+            builder.HasOne(eu => eu.Equipe).WithMany(e => e.Usuarios).HasForeignKey(eu => eu.IdEquipe);
+            builder.HasOne(eu => eu.Usuario).WithMany(e => e.Equipes).HasForeignKey(eu => eu.IdUsuario);
+        }
+    }
+}
