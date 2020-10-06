4dd37ea80ad6865deefd50a954b8822ec70e9c1c
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2020-10-06 20:48:43 -0300

    docker

--- GerenciadorDeTarefas.WebApi/Startup.cs
+++ GerenciadorDeTarefas.WebApi/Startup.cs
@@ -64,6 +64,7 @@ namespace GerenciadorDeTarefas.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
